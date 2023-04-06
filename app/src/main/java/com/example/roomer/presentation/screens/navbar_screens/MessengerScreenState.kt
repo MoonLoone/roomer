3d00ed2c4a9b1b6c802262faafc5d60e91849733
MoonLoone <mmaks02@yandex.ru>
2023-04-06 11:57:25 +0300

    Screen state

--- app/src/main/java/com/example/roomer/presentation/screens/navbar_screens/MessengerScreenState.kt
+++ app/src/main/java/com/example/roomer/presentation/screens/navbar_screens/MessengerScreenState.kt
@@ -3,9 +3,9 @@ package com.example.roomer.presentation.screens.navbar_screens
 import com.example.roomer.utils.ScreenState
 
 data class MessengerScreenState(
-    val noChats: Boolean = true,
+    val noChats: Boolean = false,
     override var success: Boolean = false,
-    override var isLoading: Boolean = true,
+    override var isLoading: Boolean = false,
     override var error: String = "Unresolved error",
     override var internetProblem: Boolean = false,
 ) : ScreenState
