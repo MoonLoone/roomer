b47c698b35465294a362c72a828f30d3b25419b4
Rodion Ivannikov <rivannikov2001@gmail.com>
2023-05-04 13:06:30 +0300

    final usecase workaround

--- app/src/main/java/com/example/roomer/utils/UtilsFunctions.kt
+++ app/src/main/java/com/example/roomer/utils/UtilsFunctions.kt
@@ -2,6 +2,7 @@ package com.example.roomer.utils
 
 object UtilsFunctions {
     fun trimString(text: String, maxLength: Int, truncateText: String = "..."): String {
+        if (maxLength < truncateText.length) return ""
         if (maxLength >= text.length) return text
         return text.substring(startIndex = 0, endIndex = maxLength - 1)
             .dropLast(truncateText.length)
